b24353d80a8024b8aef0c0cb7a7305cf12c38ff2
Vincent Nwonah <vnwonah@outlook.com>
2020-03-17 16:38:49 +0100

    modified migration and added daos

--- src/daos.lua
+++ src/daos.lua
@@ -1,1 +1,17 @@
 local typedefs = require "kong.db.schema.typedefs"
+
+return {
+    token_to_header_definitions = {
+
+        name = "token_to_header_extractor",
+        primary_key = { "id" },
+        generate_admin_api = true,
+        fields = {
+            { id = typedefs.uuid },
+            { created_at = typedefs.auto_timestamp_s },
+            { token_name = { type = "string", required = true, unique = false, auto = true }, },
+            { token_value_name = { type = "string", required = true, unique = false, auto = true }, },
+            { header_name = { type = "string", required = true, unique = false, auto = true }, },
+        },
+    },
+}
--- src/migrations/000_base_token_to_header_extractor.lua
+++ src/migrations/000_base_token_to_header_extractor.lua
@@ -1,16 +1,17 @@
 return {
   postgres = {
     up = [[
-      CREATE TABLE IF NOT EXISTS "token_to_header_extractor_table" (
+      CREATE TABLE IF NOT EXISTS "token_to_header_extractor" (
         "id"                UUID                         PRIMARY KEY,
         "created_at"        TIMESTAMP WITH TIME ZONE     DEFAULT (CURRENT_TIMESTAMP(0) AT TIME ZONE 'UTC')
+        "token_name"        TEXT
         "token_value_name"  TEXT                         
         "header_name"       TEXT                         
       );
       DO $$
       BEGIN
-        CREATE INDEX IF NOT EXISTS "token_to_header_extractor_table_token_value_name"
-                                ON "token_to_header_extractor_table" ("token_value_name");
+        CREATE INDEX IF NOT EXISTS "token_to_header_extractor_token_value_name"
+                                ON "token_to_header_extractor" ("token_value_name");
       EXCEPTION WHEN UNDEFINED_COLUMN THEN
         -- Do nothing, accept existing state
       END$$;
@@ -19,14 +20,15 @@ return {
 
   cassandra = {
     up = [[
-      CREATE TABLE IF NOT EXISTS token_to_header_extractor_table (
+      CREATE TABLE IF NOT EXISTS token_to_header_extractor (
         id                  uuid PRIMARY KEY,
         created_at          timestamp,
-        token_value_name    text                         
+        token_name          text,
+        token_value_name    text,                         
         header_name         text  
       );
       
-      CREATE INDEX IF NOT EXISTS ON token_to_header_extractor_table (token_value_name);
+      CREATE INDEX IF NOT EXISTS ON token_to_header_extractor (token_value_name);
     ]],
   }
 }
