31349cbda975af7dbf33f4e3fd1cbdafa0ad8afe
Vincent Nwonah <vnwonah@outlook.com>
2020-04-01 03:08:08 +0000

    changed schema.lua

--- kong/plugins/token-to-header-extractor/schema.lua
+++ kong/plugins/token-to-header-extractor/schema.lua
@@ -2,28 +2,19 @@ local typedefs = require "kong.db.schema.typedefs"
 
 return {
   name = "token-to-header-extractor",
+  no_consumer = true,
   fields = {
-    {
-      consumer = typedefs.no_consumer
+    token_name = { 
+      type = "string", 
+      required = true
     },
-    {
-      config = {
-        type = "record",
-        fields = {
-          token_name = { 
-            type = "string", 
-            required = true
-          },
-          token_value_name = { 
-            type = "string", 
-            required = true
-          },
-          header_name = { 
-            type = "string", 
-            required = true
-          },
-        },
-      },
+    token_value_name = { 
+      type = "string", 
+      required = true
     },
-  },
+    header_name = { 
+      type = "string", 
+      required = true
+    },
+  }
 }
