44a8bd17674a585ba3d2a6d3fc3ab7ee5cf22f7b
Vincent Nwonah <vnwonah@outlook.com>
2020-04-01 03:50:12 +0000

    temp schema.lua

--- kong/plugins/token-to-header-extractor/schema.lua
+++ kong/plugins/token-to-header-extractor/schema.lua
@@ -1,6 +1,6 @@
 local typedefs = require "kong.db.schema.typedefs"
 
-return {
+--[[ return {
   name = "token-to-header-extractor",
   fields = {
     { consumer = typedefs.no_consumer },
@@ -18,6 +18,33 @@ return {
         { header_name = { 
           type = "string", 
           required = true
+        }}
+      }
+    }}
+  }
+} ]]
+
+return {
+  name = "token-to-header-extractor",
+  fields = {
+    { consumer = typedefs.no_consumer },
+    { config = {
+      type = "record",
+      fields = {
+        { max_msg_size = {
+          type = "number",
+          required = false,
+          default = 4194304
+        }},
+        { capability_upstream = {
+           type = "map",
+           keys = { type = "string" },
+           values = { type = "string" },
+           required = false,
+           default = {
+             ["string1:substring1"] = "127.0.0.1:8082",
+             ["string2:substring2"] = "127.0.0.1:8083"
+           }
         }}
       }
     }}
