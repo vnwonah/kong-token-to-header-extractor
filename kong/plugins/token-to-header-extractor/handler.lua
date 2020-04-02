042603b740673cccaacc945f23787c49f4138d27
Vincent Nwonah <vnwonah@outlook.com>
2020-04-02 02:15:27 +0000

    iterating over entities

--- kong/plugins/token-to-header-extractor/handler.lua
+++ kong/plugins/token-to-header-extractor/handler.lua
@@ -3,6 +3,8 @@ local jwt_decoder = require "kong.plugins.jwt.jwt_parser"
 local req_set_header = ngx.req.set_header
 local ngx_re_gmatch = ngx.re.gmatch
 
+local kong = kong
+
 local TokenToHeaderExtractorHandler = BasePlugin:extend()
 
 TokenToHeaderExtractorHandler.VERSION  = "0.1.0"
@@ -19,6 +21,15 @@ function TokenToHeaderExtractorHandler:rewrite(config)
   
     -- Implement any custom logic here
     local continue_on_error = conf.continue_on_error
+
+    for entity, err on kong.db.token_to_header_extractor:each(1000) do
+        if err then
+            kong.log.err("Error when iterating over keyauth credentials: " .. err)
+            return nil
+        end
+        kong.log("token_name: " .. credential.token_name)
+    end
+
 end
 
 return TokenToHeaderExtractorHandler
